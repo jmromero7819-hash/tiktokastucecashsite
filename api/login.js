6fa59f4603db2c41da3075c2d5b24a3ef7f3926e
jmromero7819-hash <jmromero7819@gmail.com>
2026-08-19 20:22:25 +0200

    Implement TikTok OAuth login handler

---
+++ api/login.js
@@ -0,0 +1,21 @@
+export default function handler(req, res) {
+const state = crypto.randomUUID();
+
+res.setHeader(
+"Set-Cookie",
+`tiktok_oauth_state=${state}; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=600`
+);
+
+const params = new URLSearchParams({
+client_key: "awoi60q6siz0v6vk",
+scope: "user.info.basic,video.upload",
+response_type: "code",
+redirect_uri: "https://tiktokastucecashsite.vercel.app/callback",
+state,
+});
+
+res.redirect(
+302,
+`https://www.tiktok.com/v2/auth/authorize/?${params.toString()}`
+);
+}
