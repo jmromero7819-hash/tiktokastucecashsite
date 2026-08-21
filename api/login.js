16ab6d099eac9dca0c8510dfaccc299fbfe00c06
jmromero7819-hash <jmromero7819@gmail.com>
2026-08-21 23:20:35 +0200

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,22 +1,24 @@
 import crypto from "crypto";
+
 export default function handler(req, res) {
-const state = crypto.randomUUID();
+  const state = crypto.randomUUID();
 
-res.setHeader(
-"Set-Cookie",
-`tiktok_oauth_state=${state}; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=600`
-);
+  res.setHeader(
+    "Set-Cookie",
+    `tiktok_oauth_state=${state}; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=600`
+  );
 
-const params = new URLSearchParams({
-client_key: "sbawuvysjvievehtjm",
-scope: "user.info.basic,video.upload",
-response_type: "code",
-redirect_uri: "https://tiktokastucecashsite.vercel.app/api/callback",
-state,
-});
+  const params = new URLSearchParams({
+    client_key: process.env.TIKTOK_CLIENT_KEY,
+    scope: "user.info.basic,video.upload",
+    response_type: "code",
+    redirect_uri: "https://tiktokastucecashsite.vercel.app/api/callback",
+    state,
+    disable_auto_auth: "1"
+  });
 
-res.redirect(
-302,
-`https://www.tiktok.com/v2/auth/authorize/?${params.toString()}`
-);
+  res.redirect(
+    302,
+    `https://www.tiktok.com/v2/auth/authorize/?${params.toString()}`
+  );
 }
